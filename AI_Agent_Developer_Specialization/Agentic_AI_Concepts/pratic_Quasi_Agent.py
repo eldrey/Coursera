03e61c983e7ee9e07657c9666b59121f02f5e572
Eldrey Galindo <eldrey.sg@gmail.com>
2025-11-15 09:45:32 -0300

    feat: Adding code block parser

--- AI_Agent_Developer_Specialization/Agentic_AI_Concepts/pratic_Quasi_Agent.py
+++ AI_Agent_Developer_Specialization/Agentic_AI_Concepts/pratic_Quasi_Agent.py
@@ -15,6 +15,16 @@ messages = [
     {"role": "system", "content": "You are an expert software engineer."},
 ]
 
+def extract_code_block(response: str) -> str:
+    """Extract code block from response"""
+    if not '```' in response:
+        return response
+
+    code_block = response.split('```')[1].strip()
+    if code_block.startswith("python"):
+        code_block = code_block[6:]
+
+    return code_block
 
 if __name__ == "__main__":
     function = "A function that takes a word and returns all possible palindromes within that word In Python" #input("What function do you want to create? ")
@@ -23,7 +33,7 @@ if __name__ == "__main__":
     response = generate_response(messages)
     print("================ First response =================")
     print(response)
-    messages.append({"role": "assistant", "content": response})
+    messages.append({"role": "assistant", "content": extract_code_block(response)})
     messages.append({"role": "user", "content": "Update the function to include documentation."})
     print("================ Second response =================")
     response = generate_response(messages)
